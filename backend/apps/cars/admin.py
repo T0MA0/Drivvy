273eb2198e73d7a551bc6bf5e6cb27a7f15f0316
T0MA0 <bencetomasoczki@gmail.com>
2026-01-08 00:58:52 +0100

    refactor: clean up Car model by removing unused fields and updating filters in views

--- backend/apps/cars/admin.py
+++ backend/apps/cars/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Car
 
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-    list_display = ('brand', 'model', 'fuel_type', 'transmission', 'price_per_day', 'is_available')
-    list_filter = ('fuel_type', 'transmission', 'brand')
-    search_fields = ('brand', 'model')
+# Register your models here.
--- backend/apps/cars/filters.py
+++ backend/apps/cars/filters.py
@@ -2,17 +2,15 @@ import django_filters
 from .models import Car
 
 class CarFilter(django_filters.FilterSet):
-    # Lehetővé tesszük, hogy több márkát is kiválasszanak egyszerre
+    # Lehetővé teszi, hogy több értéket válasszunk (pl. ?brand=Audi&brand=BMW)
     brand = django_filters.AllValuesMultipleFilter(field_name='brand')
+    fuel_type = django_filters.AllValuesMultipleFilter(field_name='fuel_type')
+    transmission = django_filters.AllValuesMultipleFilter(field_name='transmission')
     
-    # Üzemanyag és váltó szűrés
-    fuel_type = django_filters.MultipleChoiceFilter(choices=Car.FUEL_CHOICES)
-    transmission = django_filters.MultipleChoiceFilter(choices=Car.TRANSMISSION_CHOICES)
-
-    # Ár szerinti szűrés (tól-ig), ha később kellene
+    # Ár szerinti szűrés (tól-ig)
     min_price = django_filters.NumberFilter(field_name="price_per_day", lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name="price_per_day", lookup_expr='lte')
 
     class Meta:
         model = Car
-        fields = ['brand', 'fuel_type', 'transmission', 'year']
+        fields = ['brand', 'fuel_type', 'transmission']
