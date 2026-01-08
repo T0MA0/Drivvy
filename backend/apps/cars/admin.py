2751474f47c967bd6c64911022eba9d379d9620f
T0MA0 <bencetomasoczki@gmail.com>
2026-01-08 01:15:19 +0100

    feat: update Car model and admin configuration with new filters and search fields

--- backend/apps/cars/admin.py
+++ backend/apps/cars/admin.py
@@ -4,5 +4,7 @@ from .models import Car
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'model', 'year', 'fuel_type', 'transmission', 'price_per_day', 'is_available')
-    list_filter = ('fuel_type', 'transmission', 'brand')
-    search_fields = ('brand', 'model')
+    list_filter = ('fuel_type', 'transmission', 'is_available', 'brand')
+    
+    # Keresőmező
+    search_fields = ('brand', 'model', 'license_plate')
--- backend/apps/cars/filters.py
+++ backend/apps/cars/filters.py
@@ -1,15 +1,15 @@
-import django_filters
+import django_filter
 from .models import Car
 
-class CarFilter(django_filters.FilterSet):
+class CarFilter(django_filter.FilterSet):
     # Lehetővé teszi, hogy több értéket válasszunk (pl. ?brand=Audi&brand=BMW)
-    brand = django_filters.AllValuesMultipleFilter(field_name='brand')
-    fuel_type = django_filters.AllValuesMultipleFilter(field_name='fuel_type')
-    transmission = django_filters.AllValuesMultipleFilter(field_name='transmission')
+    brand = django_filter.AllValuesMultipleFilter(field_name='brand')
+    fuel_type = django_filter.AllValuesMultipleFilter(field_name='fuel_type')
+    transmission = django_filter.AllValuesMultipleFilter(field_name='transmission')
     
     # Ár szerinti szűrés (tól-ig)
-    min_price = django_filters.NumberFilter(field_name="price_per_day", lookup_expr='gte')
-    max_price = django_filters.NumberFilter(field_name="price_per_day", lookup_expr='lte')
+    min_price = django_filter.NumberFilter(field_name="price_per_day", lookup_expr='gte')
+    max_price = django_filter.NumberFilter(field_name="price_per_day", lookup_expr='lte')
 
     class Meta:
         model = Car
