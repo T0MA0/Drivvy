89868f2d2f7ab5c74b3c3e40f9feb0249da44682
T0MA0 <bencetomasoczki@gmail.com>
2026-01-08 01:53:27 +0100

    image path changed

--- backend/apps/cars/views.py
+++ backend/apps/cars/views.py
@@ -3,32 +3,28 @@ from rest_framework import generics
 from rest_framework.response import Response
 from django.db.models import Q
 from .models import Car
-from .serializers import CarSerializer # Feltételezem, hogy van serializer
+from .serializers import CarSerializer
 from apps.bookings.models import Booking
 
 class CarListAPIView(generics.ListAPIView):
     serializer_class = CarSerializer
 
     def get_queryset(self):
-        # 1. Alapból vesszük az összes autót
         queryset = Car.objects.all()
 
-        # 2. Dátum szűrés (A legfontosabb rész!)
+
         start_date = self.request.query_params.get('start_date') # pl: 2026-01-10
         end_date = self.request.query_params.get('end_date')     # pl: 2026-01-15
 
         if start_date and end_date:
-            # Megkeressük azokat az autókat, amik FOGLALTAK ebben az időben
-            # Logika: A foglalás kezdete előbb van, mint a kérés vége ÉS a foglalás vége később van, mint a kérés kezdete
+
             booked_car_ids = Booking.objects.filter(
                 Q(start_date__lte=end_date) & Q(end_date__gte=start_date),
-                status__in=['confirmed', 'pending_payment'] # Csak az éles foglalások számítanak
+                status__in=['confirmed', 'pending_payment'] #
             ).values_list('car_id', flat=True)
 
-            # KIZÁRJUK a foglaltakat a listából
             queryset = queryset.exclude(id__in=booked_car_ids)
 
-        # 3. Bal oldali sáv szűrői (Márka, Kategória, Ár)
         brand = self.request.query_params.get('brand')
         category = self.request.query_params.get('category')
         min_price = self.request.query_params.get('min_price')
--- frontend/src/components/features/CarCard.tsx
+++ frontend/src/components/features/CarCard.tsx
@@ -13,7 +13,7 @@ export default function CarCard({ car }: CarProps) {
             {/* Kép */}
             <div style={{ position: 'relative', height: '200px', width: '100%' }}>
                 <Image 
-                    src={car.image || '/placeholder.jpg'} 
+                    src={car.image.startsWith('/') ? car.image : `/${car.image}`}
                     alt={`${car.brand} ${car.model}`}
                     fill
                     style={{ objectFit: 'cover' }}
