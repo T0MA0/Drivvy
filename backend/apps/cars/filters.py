e803e703d803ec7146236243519f047bca2e9111
T0MA0 <bencetomasoczki@gmail.com>
2026-01-08 02:06:45 +0100

    syntax_error fixed

--- backend/apps/cars/filters.py
+++ backend/apps/cars/filters.py
@@ -1,16 +1,16 @@
-import django_filter
+import django_filters
 from .models import Car
 
-class CarFilter(django_filter.FilterSet):
-    # Lehetővé teszi, hogy több értéket válasszunk (pl. ?brand=Audi&brand=BMW)
-    brand = django_filter.AllValuesMultipleFilter(field_name='brand')
-    fuel_type = django_filter.AllValuesMultipleFilter(field_name='fuel_type')
-    transmission = django_filter.AllValuesMultipleFilter(field_name='transmission')
+class CarFilter(django_filters.FilterSet):
+    # Az AllValuesMultipleFilter tökéletes a pipálós (checkbox) szűréshez!
+    brand = django_filters.AllValuesMultipleFilter(field_name='brand')
+    fuel_type = django_filters.AllValuesMultipleFilter(field_name='fuel_type')
+    transmission = django_filters.AllValuesMultipleFilter(field_name='transmission')
     
-    # Ár szerinti szűrés (tól-ig)
-    min_price = django_filter.NumberFilter(field_name="price_per_day", lookup_expr='gte')
-    max_price = django_filter.NumberFilter(field_name="price_per_day", lookup_expr='lte')
+    # Ár szűrés
+    min_price = django_filters.NumberFilter(field_name="price_per_day", lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name="price_per_day", lookup_expr='lte')
 
     class Meta:
         model = Car
-        fields = ['brand', 'fuel_type', 'transmission']
+        fields = ['brand', 'fuel_type', 'transmission', 'min_price', 'max_price']
